2e79054dba36204cb5bec4ea030271ee1a5359e3
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2026-02-05 15:28:26 -0500

    country.css

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -4,10 +4,10 @@ export function Country({ props }) {
   return (
     <div className="country">
       {props.map((country, index) => (
-        <Link key={index} to={"country-details"} state={{country}} >
-          <div >
+        <Link key={index} to={"country-details"} state={{country}} className="link-country" >
+          <div className="container-country" >
             <img src={country.flags.png} alt={country.name.common} />
-            <p>{country.name.common}</p>
+            <p className="name-common">{country.name.common}</p>
           </div>
         </Link>
       ))}
